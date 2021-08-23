913e03c1cd366b75c4d2eba8e1be95c72397ca40
LuisLopezPacheco <71840410+LuisLopezPacheco@users.noreply.github.com>
2021-08-23 17:56:48 -0500

    Update lexico.cpp
    Corrección con el final de la cadena ingresada, y se utiliza el atributo final, para manejar el último carácter ingresado.

--- Modulo1/Compilador/Compilador/lexico.cpp
+++ Modulo1/Compilador/Compilador/lexico.cpp
@@ -1,120 +1,193 @@
-#include "lexico.h"
+#include "Lexico.h"
 
-  Lexico::Lexico(string fuente){
-    ind= 0;                  
-    this->fuente= fuente;
-  }      
-
-  Lexico::Lexico(){
-    ind= 0;                  
-  }      
-  
-  string Lexico::tipoAcad(int tipo){
-         string cad= "";
-         
-         switch (tipo){
-           case TipoSimbolo::IDENTIFICADOR:
-                cad= "Identificador";
-                break;
-                
-           case TipoSimbolo::OPADIC:
-                cad= "Op. Adicion";
-                break;
-
-           case TipoSimbolo::OPMULT:
-                cad= "Op. Multiplicacion";
-                break;            
-
-           case TipoSimbolo::PESOS:
-                cad= "Fin de la Entrada";
-                break;            
-
-           case TipoSimbolo::ENTERO:
-                cad= "Entero";
-                break;
-                
-         }
-         
-         return cad;
-  }
-  
-  void Lexico::entrada(string fuente){
-       ind= 0;                  
-      this->fuente= fuente;
-  }
-
-int Lexico::sigSimbolo(){
-    
-    estado=0;
-    continua= true;
-    simbolo= "";
-    
-    //Inicio del Automata
-    while (continua){
-      c= sigCaracter();
-      
-      switch (estado){
-        case 0:
-             if ( c == '+' || c=='-') aceptacion(2);
-             else
-              if ( c == '$' ) aceptacion(3);
-
-             break;
-                                       
-      }  
-           
-   }    
-    //Fin del Automata
-    
-      switch (estado){
-              
-         case 2:
-              tipo= TipoSimbolo::OPADIC;
-              break; 
-                 
-              
-        dafault:                 
-              tipo= TipoSimbolo::ERROR;
-      }
-    		
-	return tipo;
+Lexico::Lexico(){
+	
 }
 
- char Lexico::sigCaracter(){
-      if (terminado()) return '$';
-      
-      return fuente[ind++];
- }
+void Lexico::setMyChain(const string& cadena)
+{
+	myChain = cadena;
+	size = myChain.length();
+}
 
-  void Lexico::sigEstado(int estado){
-       this->estado= estado;
-       simbolo+= c;
-  }
-  
-  void Lexico::aceptacion(int estado){
-       sigEstado(estado);
-       continua= false;
-  }
+void Lexico::Type()
+{
+	switch (state) {
+	case SymbolType::Entire:
+		cout << "Entero";
+		break;
+	case SymbolType::Lyrics:
+		cout << "Letra";
+		break;
+	case SymbolType::Point:
+		cout << "Punto";
+		break;
+	case SymbolType::Real:
+		cout << "Real";
+		break;
+	case SymbolType::Invalid:
+		cout << "Invalido";
+		break;
+	}
+	cout << endl;
+	
+}
 
-  bool Lexico::terminado(){//fin de cadena
-         return ind >= fuente.length();
-  }
+/// Inicio del automata
+void Lexico::analyze()
+{
+	state = 0;
+	index = 0;
+	next = true;
+	final = false;
+	result = "";
 
-   bool Lexico::esLetra(char c){
-        return isalpha(c) || c=='_';
-   }
-   
-   
-   bool Lexico::esDigito(char c){
-        return isdigit(c);
-   }
-   
-   bool Lexico::esEspacio(char c){
-        return c== ' ' || c== '\t';        
-   }
+	while (final == false) {
+		
+			if (next) {
+				c = nextSymbol();
+			}
 
-            
-   void Lexico::retroceso(){
-      if ( c != '$') ind--;
-        continua= false;
-    }
+			if (final == true) {
+				cout << "Final de la cadena" << endl;
+				break;
+			}
+
+			if (isNum(c) and state == 0) {
+				state = 1;
+				result += c;
+				next = true;
+			}
+			else if (isNum(c) and state == 1) {
+				result += c;
+			}
+			else if ((isLyrics(c) or c == 164) and state == 0) {
+				state = 2;
+				result += c;
+				next = true;
+			}
+			else if ((isLyrics(c) or c == 164) and state == 2) {
+				state = 2;
+				result += c;
+				next = true;
+			}
+			else if (isSpace(c)) {
+
+			}
+			else if (isPoint(c) and state == 1) {
+				state = 3;
+				result += c;
+			}
+			else if (isNum(c) and state == 3) {
+				state = 4;
+				result += c;
+			}
+			else if (isNum(c) and state == 4) {
+				result += c;
+			}
+			else if (state == -1) {
+				next = true;
+				result += c;
+			}
+			else if (isNum(c) or isLyrics(c) and state != 3) {
+				next = false;
+				behind();
+			}
+			else {
+				cout << "Se  obtuvo: " << result << " ";
+				cout << chainResult() << endl;
+				result = "";
+				cout << c << ": Invalido, interrumpio la secuecia" << endl;
+				result += c;
+				state = -1;
+			}
+
+			if (next) {
+				cout << c << ": ";
+				Type();
+			}
+
+			
+
+	}
+
+	cout << "Se  obtuvo: " << result << " ";
+	cout << chainResult() << endl;
+
+}
+
+
+char Lexico::nextSymbol()
+{
+	if (index == size) {
+		next = false;
+		final = true;
+	}
+	else {
+		return myChain[index++];
+	}
+	
+}
+
+bool Lexico::isSpace(const char& c)
+{
+	return c== ' ' || c == '\t';
+}
+
+bool Lexico::finaly()
+{
+	
+	return false;
+}
+
+bool Lexico::isNum(const char& c)
+{
+	return isdigit(c);
+}
+
+bool Lexico::isLyrics(const char& c)
+{
+	return isalpha(c) || c == 'ñ' || c== 'Ñ';
+}
+
+bool Lexico::isPoint(const char& c)
+{
+	return c== '.';
+}
+
+string Lexico::chainResult()
+{
+	string cadena;
+	switch (state) {
+	case SymbolType::Entire:
+		cout << "Entero";
+		break;
+	case SymbolType::Lyrics:
+		cout << "Letras";
+		break;
+	case SymbolType::Point:
+		cout << "Punto";
+		break;
+	case SymbolType::Real:
+		cout << "Real";
+		break;
+	case SymbolType::Invalid:
+		cout << "Invalido";
+		break;
+	default:
+		cout << "Invalido";
+		break;
+	}
+
+	return cadena;
+}
+
+void Lexico::behind()
+{
+	cout << "Se  obtuvo: " << result << " ";
+	cout << chainResult() << endl;
+	result = "";
+	cout << c << ": Interrumpio la secuecia, vuelve al estado inicial" << endl;
+	state = 0;
+	
+}
